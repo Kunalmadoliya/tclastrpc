9c43aba62fc2af8007ee7baf0a161ab135cf236a
Kunal Madoliya <madoliyakunal2005@gmail.com>
2026-05-21 23:50:18 +0530

    feat: implement tRPC package with router and client integration

--- apps/api/dist/index.js
+++ apps/api/dist/index.js
@@ -5,19 +5,19 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 const PORT = 5000;
-app.get("/", (req, res) => {
-    return res.json("app is working");
-});
-app.post("/user", async (req, res) => {
-    const result = createUserSchema.safeParse(req.body);
-    //fix all error
-    if (!result.success) {
-        return res.status(400).json({ message: "Invalid user data" });
-    }
-    const { name, email, password } = result.data;
-    console.log(name, email, password);
-    res.json({ message: "User created successfully" });
-});
+// app.get("/", (req, res) => {
+//   return res.json("app is working");
+// });
+// app.post("/user", async (req, res) => {
+//   const result = createUserSchema.safeParse(req.body);
+//   //fix all error
+//   if (!result.success) {
+//     return res.status(400).json({ message: "Invalid user data" });
+//   }
+//   const { name, email, password } = result.data;
+//   console.log(name, email, password);
+//   res.json({ message: "User created successfully" });
+// });
 app.listen(PORT, () => {
     console.log(`Server started on port http://localhost:${PORT}`);
 });
--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,30 +1,29 @@
 import express from "express";
 import { createUserSchema } from "@tclastrpc/utils";
-import cors from "cors";
-
+import cors from "cors"
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 const PORT = 5000;
 
-app.get("/", (req, res) => {
-  return res.json("app is working");
-});
+// app.get("/", (req, res) => {
+//   return res.json("app is working");
+// });
 
-app.post("/user", async (req, res) => {
-  const result = createUserSchema.safeParse(req.body);
+// app.post("/user", async (req, res) => {
+//   const result = createUserSchema.safeParse(req.body);
 
-  //fix all error
-  if (!result.success) {
-    return res.status(400).json({ message: "Invalid user data" });
-  }
+//   //fix all error
+//   if (!result.success) {
+//     return res.status(400).json({ message: "Invalid user data" });
+//   }
 
-  const { name, email, password } = result.data;
+//   const { name, email, password } = result.data;
 
-  console.log(name, email, password);
-  res.json({ message: "User created successfully" });
-});
+//   console.log(name, email, password);
+//   res.json({ message: "User created successfully" });
+// });
 
 app.listen(PORT, () => {
   console.log(`Server started on port http://localhost:${PORT}`);
